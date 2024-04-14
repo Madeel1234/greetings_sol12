31f1cae0db831a8647668b62302e4aa00f03fcef
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 10:40:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Greetings: start with the array used inexercise 11, but instead of just printing each persons name printing a message to them.
+// The text of each message should be the same , but each message should be personlizd with the persons name.
+let members = ["Aslam", "Noushad", "Adeel", "Ahmer"];
+let message = 'Kal birthday hay kis ki';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
+;
+export {};
